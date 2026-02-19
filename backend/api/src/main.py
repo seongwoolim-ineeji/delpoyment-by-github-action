8a86989e9e85563a1c75dfda3d506269e711d03d
Seongwoo Lim <252972047+seongwoolim-ineeji@users.noreply.github.com>
2026-02-19 16:30:38 +0900

    initial commit

---
+++ backend/api/src/main.py
@@ -0,0 +1,20 @@
+import fastapi
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+
+app = fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
